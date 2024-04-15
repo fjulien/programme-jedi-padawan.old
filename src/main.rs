65d86625ffa9b1f00e3d1af21457948f3265a5e1
fjulien <39367571+fjulien@users.noreply.github.com>
2024-04-15 16:55:08 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,5 @@ fn main() {
     // set up logging
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-
-    mount_to_body(|| {
-        
-        view! {
-            <App  />
-        }
-    })
+    mount_to_body(|| view! { <App/> })
 }
