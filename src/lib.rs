3cf5e73582402291ddedb4a69ec8a9a00a3c7cbc
fjulien <39367571+fjulien@users.noreply.github.com>
2024-04-15 17:37:14 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -43,7 +43,7 @@ pub fn App() -> impl IntoView {
                     <main class="mt-[74px]">
                         <Routes>
                             <Route path="/" view=Home/>
-                            <Route path="/*" fallback=|| view! { NotFound }/>
+                            <Route path="/*" view=|| view! { NotFound }/>
                         </Routes>
                     </main>
                 </Router>
