52222a5aaee7e3a85399d9618e56e2ad909e88db
fjulien <39367571+fjulien@users.noreply.github.com>
2024-04-15 17:43:22 +0200

    Update navigation_link.rs

--- src/components/navigation_link.rs
+++ src/components/navigation_link.rs
@@ -3,7 +3,7 @@ use leptos_router::*;
 
 #[component]
 pub fn navigation_link(link: String) -> impl IntoView {
-  let anchor_link: String = format!("/#{}", link.clone());
+  let anchor_link: String = format!("#{}", link.clone());
   view!{
     <li class="my-3 capitalize"><A href=anchor_link>{link}</A></li>
   }
