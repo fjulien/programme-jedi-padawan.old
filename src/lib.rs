5904c9803b38721558e90a46edfbfb6d0331ef4f
fjulien <39367571+fjulien@users.noreply.github.com>
2024-04-16 11:59:18 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ pub fn App() -> impl IntoView {
             // injects metadata in the <head> of the page
             <Meta charset="UTF-8"/>
             <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <Router base="programme-jedi-padawan">
+            <Router>
             <Navigation/>
                 <main class="mt-[74px]">
                     <Routes>
