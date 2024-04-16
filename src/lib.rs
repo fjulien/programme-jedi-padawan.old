2fcb6aff1167234b5988058169ea142bad36edfa
fjulien <39367571+fjulien@users.noreply.github.com>
2024-04-16 11:39:57 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -17,36 +17,19 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-            <ErrorBoundary fallback=|errors| {
-                view! {
-                    <h1>"Uh oh! Something went wrong!"</h1>
-                    <p>"Errors: "</p>
-                    // Render a list of errors as strings - good for development purposes
-                    <ul>
-                        {move || {
-                            errors
-                                .get()
-                                .into_iter()
-                                .map(|(_, e)| view! { <li>{e.to_string()}</li> })
-                                .collect_view()
-                        }}
-                    </ul>
-                }
-            }>
-                <Html lang="fr" dir="ltr" attr:data-theme="dark"/>
-                <Title text="Programme Jedi Padawan"/>
-                // injects metadata in the <head> of the page
-                <Meta charset="UTF-8"/>
-                <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <Router base="programme-jedi-padawan">
-                <Navigation/>
-                    <main class="mt-[74px]">
-                        <Routes>
-                            <Route path="/" view=Home/>
-                            <Route path="/*" view=NotFound/>
-                        </Routes>
-                    </main>
-                </Router>
-            </ErrorBoundary>
+            <Html lang="fr" dir="ltr" attr:data-theme="dark"/>
+            <Title text="Programme Jedi Padawan"/>
+            // injects metadata in the <head> of the page
+            <Meta charset="UTF-8"/>
+            <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <Router base="programme-jedi-padawan">
+            <Navigation/>
+                <main class="mt-[74px]">
+                    <Routes>
+                        <Route path="/" view=Home/>
+                        <Route path="/*" view=NotFound/>
+                    </Routes>
+                </main>
+            </Router>
         }
 }
