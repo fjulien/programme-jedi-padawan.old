b56dffaa36691fda867deffb665ade05ec0e639c
fjulien <fettingerjulien@yahoo.fr>
2024-04-19 20:32:47 +0000

    save

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+extern crate console_error_panic_hook;
+
 use leptos::*;
 use programme_jedi_padawan::App;
+
 
 // Modules
 pub mod components;
 
 
 fn main() {
-    // set up logging
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
     mount_to_body(|| view! { <App/> })
