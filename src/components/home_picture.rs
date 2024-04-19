6fe04fd56a7c57c571bd932542e8594c7029ef88
fjulien <fettingerjulien@yahoo.fr>
2024-04-19 20:40:30 +0000

    save

--- src/components/home_picture.rs
+++ src/components/home_picture.rs
@@ -5,7 +5,7 @@ pub fn home_picture() -> impl IntoView {
     view! {
       <section class="flex">
         <div></div>
-      <img src="../../images/entrainement-padawan-2.jpg"
+      <img src="/images/entrainement-padawan-2.jpg"
             alt="Entrainement d'un jadi et d'un padawan"/>
       </section> 
     }
