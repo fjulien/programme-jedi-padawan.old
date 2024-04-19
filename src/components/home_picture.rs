dad1fe5791caba2eafa120feb44ab1e59c46ebab
fjulien <fettingerjulien@yahoo.fr>
2024-04-19 11:32:57 +0000

    save

--- src/components/home_picture.rs
+++ src/components/home_picture.rs
@@ -2,9 +2,11 @@ use leptos::*;
 
 #[component]
 pub fn home_picture() -> impl IntoView {
-  view!{
-    <section class="bg-cover bg-center h-[calc(100vh-74px)]"
-        style="background-image: url(\"https://raw.githubusercontent.com/fjulien/programme-jedi-padawan/main/public/images/entrainement-padawan-2.jpg\")">
-    </section>
-  }
+    view! {
+      <section class="flex">
+        <div></div>
+        <img src="programme-jedi-padawan/main/public/images/entrainement-padawan-2.jpg"
+            alt="Entrainement d'un jadi et d'un padawan"/>
+      </section> 
+    }
 }
