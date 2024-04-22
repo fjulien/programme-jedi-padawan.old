1b16c76546f3a427c015d0549bf896dc2014f6b1
fjulien <fettingerjulien@yahoo.fr>
2024-04-22 10:43:43 +0000

    save

--- src/components/home_picture.rs
+++ src/components/home_picture.rs
@@ -6,7 +6,7 @@ pub fn home_picture() -> impl IntoView {
     view! {
       <section class="flex">
         <div></div>
-      <img src="programme-jedi-padawan/public/images/entrainement-padawan-2.jpg"
+      <img src="public/images/entrainement-padawan-2.jpg"
       blur=true
       width=750
       height=500
