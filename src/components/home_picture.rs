92fb2a7af67afb02ae6409a2ef2dc8f0d3d8ac5e
fjulien <fettingerjulien@yahoo.fr>
2024-04-22 10:34:00 +0000

    save

--- src/components/home_picture.rs
+++ src/components/home_picture.rs
@@ -1,11 +1,16 @@
 use leptos::*;
+use leptos_image::*;
 
 #[component]
 pub fn home_picture() -> impl IntoView {
     view! {
       <section class="flex">
         <div></div>
-      <img src="/images/entrainement-padawan-2.jpg"
+      <img src="programme-jedi-padawan/public/images/entrainement-padawan-2.jpg"
+      blur=true
+      width=750
+      height=500
+      quality=85
             alt="Entrainement d'un jadi et d'un padawan"/>
       </section> 
     }
--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,5 @@ pub mod components;
 fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <App/> })
+    leptos::mount_to_body(App);
 }
